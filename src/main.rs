e9c1b1495f5d9b20e37af685be31f5ae8c3b18ed
Silas Sadia <silaspsadia@gmail.com>
2022-04-26 14:42:14 -0700

    Add left movement component and incorporate it into system

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,27 @@ use std::cmp::{max, min};
 use specs_derive::Component;
 
 #[derive(Component)]
+struct LeftMover {
+
+}
+
+struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>,
+                       WriteStorage<'a, Position>);
+ 
+    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 {
+                pos.x = 79;
+            }
+        }
+    }
+}
+
+#[derive(Component)]
 struct Position {
     x: i32,
     y: i32,
@@ -22,12 +43,22 @@ struct State {
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        self.run_systems();
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
         
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
+    }
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker{};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
     }
 }
 
@@ -42,6 +73,7 @@ fn main() -> rltk::BError {
 
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
 
     gs.ecs
         .create_entity()
@@ -62,6 +94,7 @@ fn main() -> rltk::BError {
             fg: RGB::named(rltk::RED),
             bg: RGB::named(rltk::BLACK),
         })
+        .with(LeftMover{})
         .build();
     }
 
