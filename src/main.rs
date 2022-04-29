919dc38495259e04164280925ee220e0213156bd
Silas Sadia <silaspsadia@gmail.com>
2022-04-29 15:36:19 -0700

    Add rooms and corridors to the map and additional key codes

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod map;
 pub use map::*;
 mod player;
 pub use player::*;
+mod rect;
+pub use rect::Rect;
 
 pub struct State {
    pub ecs: World,
@@ -51,11 +53,13 @@ fn main() -> rltk::BError {
     gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
 
-    gs.ecs.insert(new_map());
+    let (rooms, map) = new_map_rooms_and_corridors();
+    gs.ecs.insert(map);
+    let (player_x, player_y) = rooms[0].center();
 
     gs.ecs
         .create_entity()
-        .with(Position { x: STARTING_X, y: STARTING_Y })
+        .with(Position { x: player_x, y: player_y })
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
